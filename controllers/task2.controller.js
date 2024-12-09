125d8b09b130bec3c4a343737b6bd83f987bd179
HoangLong2507 <longnguyendt04@gmail.com>
2024-12-10 02:22:33 +0700

    Update task2.2

--- controllers/task2.controller.js
+++ controllers/task2.controller.js
@@ -10,8 +10,8 @@ export default class Task2 {
       );
       const sql = "SELECT * from CUSTOMER";
       const [customer] = await pool.execute(sql);
-      const exist_cus=customer.filter((cus)=>cus.ID===ID);
-      if(result.length===0&&exist_cus.length===0) {
+      const exist_cus = customer.filter((cus) => cus.ID === ID);
+      if (result.length === 0 && exist_cus.length === 0) {
         return res.status(404).json({
           status: "error",
           message: "Customer not found",
@@ -32,19 +32,21 @@ export default class Task2 {
   async getordersbycategory(req, res, next) {
     try {
       const { ID } = req.params;
+      console.log(ID);
       const [[result]] = await pool.execute(
         "CALL RetrieveOrdersByCategory(?)",
         [ID]
       );
-      const sql = "SELECT * from CUSTOMER";
+      const sql = "SELECT * from category";
       const [category] = await pool.execute(sql);
-      const exist_category=category.filter((cate)=>cate.ID===ID);
-      if(result.length===0&&exist_category.length===0) {
+      const exist_category = category.filter((cate) => cate.ID == ID);
+      if (exist_category.length === 0) {
         return res.status(404).json({
           status: "error",
           message: "Category not found",
         });
       }
+
       res.status(200).json({
         status: "success",
         result,
@@ -75,7 +77,10 @@ export default class Task2 {
 
   async getcategoryID(req, res, next) {
     try {
-      const sql = "SELECT * from category";
+      const sql = `
+SELECT c.ID, c.name AS categoryName, r.name AS resName
+FROM category c
+JOIN restaurant r ON c.RID=r.ID`;
       const [result] = await pool.execute(sql);
       res.status(200).json({
         status: "success",
@@ -95,12 +100,12 @@ export default class Task2 {
       const sqlDeleteOrder = `DELETE FROM food_order WHERE ID=?`;
 
       const sql = "SELECT * FROM food_order WHERE id = ?";
-      const [result] = await pool.execute(sql,[ID]);
+      const [result] = await pool.execute(sql, [ID]);
 
-      if(result.length===0) {
+      if (result.length === 0) {
         return res.status(404).json({
-          status:"error",
-          message: "Order not found"
+          status: "error",
+          message: "Order not found",
         });
       }
       //Delete food item
